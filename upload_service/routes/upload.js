2c25a83c1d1d4896d315c7a1ec9ae3cafa1dce2e
BossLogic314 <bhagavatulaanish@gmail.com>
2024-05-26 00:16:26 +0530

    Minor change

--- upload_service/routes/upload.js
+++ upload_service/routes/upload.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { uploadVideo } from "../controller/upload.js";
+import multer from "multer";
+import { createMultipartUpload, uploadChunk, completeMultipartUpload } from "../controllers/upload.js";
 
+const upload = multer();
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
-router.post('/uploadVideo', jsonParser, uploadVideo);
+router.post('/createMultipartUpload', jsonParser, createMultipartUpload);
+router.post('/uploadChunk', jsonParser, upload.single('chunk'), uploadChunk);
+router.post('/completeMultipartUpload', jsonParser, completeMultipartUpload);
 
 export default router;
