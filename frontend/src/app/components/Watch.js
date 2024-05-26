296802e19e13d70739dfa61fffbc865e53221974
BossLogic314 <bhagavatulaanish@gmail.com>
2024-05-26 20:42:10 +0530

    Updated UI to display videos

--- frontend/src/app/components/Watch.js
+++ frontend/src/app/components/Watch.js
@@ -25,18 +25,24 @@ export default function Watch() {
     console.log(videos);
     return (
         <div className="home h-screen w-screen overflow-y-scroll">
-            {
-                videos == null ? <></> :
-                (
-                    videos.map((element) => (
-                        <div className="videoDiv" key={element.id}>
-                            <ReactPlayer
-                                url={element.url} width="360px" height="180px" controls={true}
-                            />
-                        </div>
-                    ))
-                )
-            }
+            <div className="videos border-black border-[2px]">
+                {
+                    videos == null ? <></> :
+                    (
+                        videos.map((element) => (
+                            <div className="videoDiv w-[360px] mt-[10px] ml-[10px] border-black border-[1px] inline-block" key={element.id}>
+                                <ReactPlayer
+                                    url={element.url} width="360px" height="180px" controls={true}
+                                />
+
+                                <div className="title font-[700] text-[25px] ml-[4px]">{element.title}</div>
+                                <div className="author font-[400] text-[18px] ml-[4px]">{element.author}</div>
+                                <div className="description font-[400] ml-[4px] text-[18px]">{element.description}</div>
+                            </div>
+                        ))
+                    )
+                }
+            </div>
         </div>
     )
 }
