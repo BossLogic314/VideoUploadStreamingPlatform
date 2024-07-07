31724ca7ea05cd0e5369f5068983fe471629d462
BossLogic314 <bhagavatulaanish@gmail.com>
2024-07-08 00:38:26 +0530

    Changed the opensearch service which is being used in this application

--- watch_service/opensearch/connect.js
+++ watch_service/opensearch/connect.js
@@ -4,7 +4,7 @@ export let openSearchClient = null;
 
 export const connectToOpenSearchService = async() => {
     openSearchClient = new Client({
-        node: `https://${process.env.OPEN_SEARCH_USERNAME}:${process.env.OPEN_SEARCH_PASSWORD}@os-7491aba-videostreaminguploadplatform.f.aivencloud.com:22906`,
+        node: `https://${process.env.OPEN_SEARCH_USERNAME}:${process.env.OPEN_SEARCH_PASSWORD}@os-14f9b1a3-videostreaminguploadplatform.i.aivencloud.com:22906`,
     });
     console.log('Connected to open search service');
 }
